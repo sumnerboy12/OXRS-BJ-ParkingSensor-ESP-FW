1205a36db9120f1dd21f1e02ba3d493e6f9e92ce
Ben Jones <sumnerboy12@users.noreply.github.com>
2023-02-17 22:26:14 +1300

    switch to oxrs hardware libs (#4)

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 /**
-  ESP8266 Parking sensor firmware for the Open eXtensible Rack System
+  Parking sensor firmware for the Open eXtensible Rack System
   
   GitHub repository:
     https://github.com/sumnerboy12/OXRS-BJ-ParkingSensor-ESP-FW
@@ -7,25 +7,23 @@
   Copyright 2022 Ben Jones <ben.jones12@gmail.com>
 */
 
-/*--------------------------- Macros ----------------------------------*/
-#define STRINGIFY(s) STRINGIFY1(s)
-#define STRINGIFY1(s) #s
-
 /*--------------------------- Libraries -------------------------------*/
-#include <ESP8266WiFi.h>
-#include <WiFiManager.h>
-#include <OXRS_MQTT.h>
-#include <OXRS_API.h>
-#include <MqttLogger.h>
 #include <Adafruit_NeoPixel.h>
 #include <NewPing.h>
+
+#if defined(OXRS_ESP32)
+#include <OXRS_32.h>                  // ESP32 support
+OXRS_32 oxrs;
+
+#elif defined(OXRS_ESP8266)
+#include <OXRS_8266.h>                // ESP8266 support
+OXRS_8266 oxrs;
+
+#endif
 
 /*--------------------------- Constants -------------------------------*/
 // Serial
 #define     SERIAL_BAUD_RATE          115200
-
-// REST API
-#define     REST_API_PORT             80
 
 // Sensor config
 #define     MAX_DISTANCE_CM           300 // max sensor distance is rated at 400-500cm
@@ -81,24 +79,7 @@ int parked            = -1;
 uint32_t lastFlashMs = 0;
 bool lastFlashState = false;
 
-// stack size counter
-char * stackStart;
-
 /*--------------------------- Instantiate Globals ---------------------*/
-// WiFi client
-WiFiClient _client;
-
-// MQTT client
-PubSubClient _mqttClient(_client);
-OXRS_MQTT _mqtt(_mqttClient);
-
-// REST API
-WiFiServer _server(REST_API_PORT);
-OXRS_API _api(_mqtt);
-
-// Logging
-MqttLogger _logger(_mqttClient, "log", MqttLoggerMode::MqttAndSerial);
-
 // RGB LED
 Adafruit_NeoPixel _pixels = Adafruit_NeoPixel(1, RGB_PIN, NEO_GRB + NEO_KHZ800);
 
@@ -106,101 +87,30 @@ Adafruit_NeoPixel _pixels = Adafruit_NeoPixel(1, RGB_PIN, NEO_GRB + NEO_KHZ800);
 NewPing _sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE_CM);
 
 /*--------------------------- Program ---------------------------------*/
-uint32_t getStackSize()
+void setConfigSchema()
 {
-  char stack;
-  return (uint32_t)stackStart - (uint32_t)&stack;  
-}
-
-void getFirmwareJson(JsonVariant json)
-{
-  JsonObject firmware = json.createNestedObject("firmware");
-
-  firmware["name"] = FW_NAME;
-  firmware["shortName"] = FW_SHORT_NAME;
-  firmware["maker"] = FW_MAKER;
-  firmware["version"] = STRINGIFY(FW_VERSION);
-
-#if defined(FW_GITHUB_URL)
-  firmware["githubUrl"] = FW_GITHUB_URL;
-#endif
-}
-
-void getSystemJson(JsonVariant json)
-{
-  JsonObject system = json.createNestedObject("system");
-
-  system["heapUsedBytes"] = getStackSize();
-  system["heapFreeBytes"] = ESP.getFreeHeap();
-  system["flashChipSizeBytes"] = ESP.getFlashChipSize();
-
-  system["sketchSpaceUsedBytes"] = ESP.getSketchSize();
-  system["sketchSpaceTotalBytes"] = ESP.getFreeSketchSpace();
-
-  FSInfo fsInfo;
-  SPIFFS.info(fsInfo);
+  // Define our config schema
+  StaticJsonDocument<1024> json;
   
-  system["fileSystemUsedBytes"] = fsInfo.usedBytes;
-  system["fileSystemTotalBytes"] = fsInfo.totalBytes;
-}
-
-void getNetworkJson(JsonVariant json)
-{
-  byte mac[6];
-  WiFi.macAddress(mac);
-  
-  char mac_display[18];
-  sprintf_P(mac_display, PSTR("%02X:%02X:%02X:%02X:%02X:%02X"), mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-
-  JsonObject network = json.createNestedObject("network");
-
-  network["mode"] = "wifi";
-  network["ip"] = WiFi.localIP();
-  network["mac"] = mac_display;
-}
-
-void getConfigSchemaJson(JsonVariant json)
-{
-  JsonObject configSchema = json.createNestedObject("configSchema");
-  
-  // Config schema metadata
-  configSchema["$schema"] = JSON_SCHEMA_VERSION;
-  configSchema["title"] = FW_SHORT_NAME;
-  configSchema["type"] = "object";
-
-  JsonObject properties = configSchema.createNestedObject("properties");
-
-  JsonObject debug = properties.createNestedObject("debug");
+  JsonObject debug = json.createNestedObject("debug");
   debug["type"] = "boolean";
 
-  JsonObject pingAverage = properties.createNestedObject("pingAverage");
+  JsonObject pingAverage = json.createNestedObject("pingAverage");
   pingAverage["type"] = "integer";
   pingAverage["minimum"] = 1;
   pingAverage["maximum"] = PING_AVERAGE_MAX;
 
-  JsonObject maxDistance = properties.createNestedObject("maxDistance");
+  JsonObject maxDistance = json.createNestedObject("maxDistance");
   maxDistance["type"] = "integer";
 
-  JsonObject parkDistance = properties.createNestedObject("parkDistance");
+  JsonObject parkDistance = json.createNestedObject("parkDistance");
   parkDistance["type"] = "integer";
 
-  JsonObject parkRange = properties.createNestedObject("parkRange");
+  JsonObject parkRange = json.createNestedObject("parkRange");
   parkRange["type"] = "integer";
-}
 
-void getCommandSchemaJson(JsonVariant json)
-{
-  JsonObject commandSchema = json.createNestedObject("commandSchema");
-  
-  // Command schema metadata
-  commandSchema["$schema"] = JSON_SCHEMA_VERSION;
-  commandSchema["title"] = FW_SHORT_NAME;
-  commandSchema["type"] = "object";
-
-  JsonObject properties = commandSchema.createNestedObject("properties");
-
-  JsonObject restart = properties.createNestedObject("restart");
-  restart["type"] = "boolean";
+  // Pass our config schema down to the hardware library
+  oxrs.setConfigSchema(json.as<JsonVariant>());
 }
 
 // calculate our thresholds etc
@@ -212,17 +122,17 @@ void updateThresholds()
   parkApproach  = parkDistance + (3 * parkRange);
 
   // debugging
-  _logger.print(F("[park] park distance: "));
-  _logger.print(parkDistance);
-  _logger.print(F("cm; range +/-"));
-  _logger.print(parkRange);
-  _logger.print(F("cm; (upper "));
-  _logger.print(parkUpper);
-  _logger.print(F("cm; lower "));
-  _logger.print(parkLower);
-  _logger.print(F("cm; approach "));
-  _logger.print(parkApproach);
-  _logger.println(F("cm)"));
+  oxrs.print(F("[park] park distance: "));
+  oxrs.print(parkDistance);
+  oxrs.print(F("cm; range +/-"));
+  oxrs.print(parkRange);
+  oxrs.print(F("cm; (upper "));
+  oxrs.print(parkUpper);
+  oxrs.print(F("cm; lower "));
+  oxrs.print(parkLower);
+  oxrs.print(F("cm; approach "));
+  oxrs.print(parkApproach);
+  oxrs.println(F("cm)"));
 }
 
 bool getFlashState()
@@ -262,80 +172,7 @@ void flashBlue()
   if (getFlashState()) blue(); else off(); 
 }
 
-/**
-  API callbacks
-*/
-void _apiAdopt(JsonVariant json)
-{
-  // Build device adoption info
-  getFirmwareJson(json);
-  getSystemJson(json);
-  getNetworkJson(json);
-  getConfigSchemaJson(json);
-  getCommandSchemaJson(json);
-}
-
-/**
-  MQTT callbacks
-*/
-void _mqttConnected()
-{
-  // MqttLogger doesn't copy the logging topic to an internal
-  // buffer so we have to use a static array here
-  static char logTopic[64];
-  _logger.setTopic(_mqtt.getLogTopic(logTopic));
-
-  // Publish device adoption info
-  DynamicJsonDocument json(JSON_ADOPT_MAX_SIZE);
-  _mqtt.publishAdopt(_api.getAdopt(json.as<JsonVariant>()));
-
-  // Log the fact we are now connected
-  _logger.println("[park] mqtt connected");
-}
-
-void _mqttDisconnected(int state) 
-{
-  // Log the disconnect reason
-  // See https://github.com/knolleary/pubsubclient/blob/2d228f2f862a95846c65a8518c79f48dfc8f188c/src/PubSubClient.h#L44
-  switch (state)
-  {
-    case MQTT_CONNECTION_TIMEOUT:
-      _logger.println(F("[park] mqtt connection timeout"));
-      break;
-    case MQTT_CONNECTION_LOST:
-      _logger.println(F("[park] mqtt connection lost"));
-      break;
-    case MQTT_CONNECT_FAILED:
-      _logger.println(F("[park] mqtt connect failed"));
-      break;
-    case MQTT_DISCONNECTED:
-      _logger.println(F("[park] mqtt disconnected"));
-      break;
-    case MQTT_CONNECT_BAD_PROTOCOL:
-      _logger.println(F("[park] mqtt bad protocol"));
-      break;
-    case MQTT_CONNECT_BAD_CLIENT_ID:
-      _logger.println(F("[park] mqtt bad client id"));
-      break;
-    case MQTT_CONNECT_UNAVAILABLE:
-      _logger.println(F("[park] mqtt unavailable"));
-      break;
-    case MQTT_CONNECT_BAD_CREDENTIALS:
-      _logger.println(F("[park] mqtt bad credentials"));
-      break;      
-    case MQTT_CONNECT_UNAUTHORIZED:
-      _logger.println(F("[park] mqtt unauthorised"));
-      break;      
-  }
-}
-
-void _mqttCallback(char * topic, byte * payload, int length)
-{
-  // Pass down to our MQTT handler
-  _mqtt.receive(topic, payload, length);
-}
-
-void _mqttConfig(JsonVariant json)
+void jsonConfig(JsonVariant json)
 {
   bool update = false;
   
@@ -367,14 +204,6 @@ void _mqttConfig(JsonVariant json)
   }
 
   if (update){ updateThresholds(); }
-}
-
-void _mqttCommand(JsonVariant json)
-{
-  if (json.containsKey("restart") && json["restart"].as<bool>())
-  {
-    ESP.restart();
-  }
 }
 
 void publishParkState()
@@ -418,7 +247,7 @@ void publishParkState()
       break;
   }
   
-  _mqtt.publishStatus(json.as<JsonVariant>());
+  oxrs.publishStatus(json.as<JsonVariant>());
 }
 
 void updateParkState(int newState)
@@ -487,7 +316,7 @@ void publishDistance(int measured)
 
   StaticJsonDocument<32> json;
   json["distance"] = publish;
-  _mqtt.publishTelemetry(json.as<JsonVariant>());
+  oxrs.publishTelemetry(json.as<JsonVariant>());
   
   lastPublish = publish;
 }
@@ -553,93 +382,18 @@ void checkDistance(int measured)
 /**
   Initialisation
 */
-void initialiseSerial()
-{
-  Serial.begin(SERIAL_BAUD_RATE);
-  delay(1000);
-  
-  _logger.println(F("[park] starting up..."));
-
-  DynamicJsonDocument json(128);
-  getFirmwareJson(json.as<JsonVariant>());
-
-  _logger.print(F("[park] "));
-  serializeJson(json, _logger);
-  _logger.println();
-}
-
-void initialseWifi(byte * mac)
-{
-  // Ensure we are in the correct WiFi mode
-  WiFi.mode(WIFI_STA);
-
-  // Connect using saved creds, or start captive portal if none found
-  // Blocks until connected or the portal is closed
-  WiFiManager wm;  
-  if (!wm.autoConnect("OXRS_WiFi", "superhouse"))
-  {
-    // If we are unable to connect then restart
-    ESP.restart();
-  }
-  
-  // Get ESP8266 base MAC address
-  WiFi.macAddress(mac);
-
-  // Format the MAC address for display
-  char mac_display[18];
-  sprintf_P(mac_display, PSTR("%02X:%02X:%02X:%02X:%02X:%02X"), mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-
-  // Display MAC/IP addresses on serial
-  _logger.print(F("[park] mac address: "));
-  _logger.println(mac_display);  
-  _logger.print(F("[park] ip address: "));
-  _logger.println(WiFi.localIP());
-}
-
-void initialiseMqtt(byte * mac)
-{
-  // Set the default client id to the last 3 bytes of the MAC address
-  char clientId[32];
-  sprintf_P(clientId, PSTR("%02x%02x%02x"), mac[3], mac[4], mac[5]);  
-  _mqtt.setClientId(clientId);
-  
-  // Register our callbacks
-  _mqtt.onConnected(_mqttConnected);
-  _mqtt.onDisconnected(_mqttDisconnected);
-  _mqtt.onConfig(_mqttConfig);
-  _mqtt.onCommand(_mqttCommand);
-  
-  // Start listening for MQTT messages
-  _mqttClient.setCallback(_mqttCallback);  
-}
-
-void initialiseRestApi(void)
-{
-  // NOTE: this must be called *after* initialising MQTT since that sets
-  //       the default client id, which has lower precendence than MQTT
-  //       settings stored in file and loaded by the API
-
-  // Set up the REST API
-  _api.begin();
-
-  // Register our callbacks
-  _api.onAdopt(_apiAdopt);
-
-  _server.begin();
-}
-
 void initialiseRgb()
 {
-  _logger.println("[park] initialising RGB LED...");
+  oxrs.println("[park] initialising RGB LED...");
   _pixels.begin();
 
-  _logger.println(" - red test");
+  oxrs.println(" - red test");
   red();
   delay(1000);
-  _logger.println(" - green test");
+  oxrs.println(" - green test");
   green();
   delay(1000);
-  _logger.println(" - blue test");
+  oxrs.println(" - blue test");
   blue();
   delay(1000);  
 }
@@ -661,29 +415,21 @@ void initialiseSonar()
 */
 void setup() 
 {
-  // Store the address of the stack at startup so we can determine
-  // the stack size at runtime (see getStackSize())
-  char stack;
-  stackStart = &stack;
-  
-  // Set up serial
-  initialiseSerial();  
+  Serial.begin(SERIAL_BAUD_RATE);
+  delay(1000);  
+  Serial.println(F("[park] starting up..."));
 
-  // Set up network and obtain an IP address
-  byte mac[6];
-  initialseWifi(mac);
-
-  // initialise MQTT
-  initialiseMqtt(mac);
-
-  // Set up the REST API
-  initialiseRestApi();
+  // Start hardware
+  oxrs.begin(jsonConfig, NULL);
 
   // Set up the RGB
   initialiseRgb();
 
   // Set up the sonar
   initialiseSonar();
+
+  // Set up the config schema (for self-discovery and adoption)
+  setConfigSchema();
 }
 
 /**
@@ -691,12 +437,8 @@ void setup()
 */
 void loop() 
 {
-  // Check our MQTT broker connection is still ok
-  _mqtt.loop();
-
-  // Handle any REST API requests
-  WiFiClient client = _server.available();
-  _api.loop(&client);
+  // Let hardware handle any events etc
+  oxrs.loop();
 
   // measure the distance (in cm) - defaults to maxDistance if no reading
   int measured = pingDistance();
